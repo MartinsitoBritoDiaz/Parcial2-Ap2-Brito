66a5044aca62a8a8471eb4d249779a19d61abf22
Martinsito Brito Diaz <martinsito_brito@ucne.edu.do>
2020-07-08 14:46:32 -0400

    Add project files.

---
+++ Parcial2-Ap2-Brito/DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial2_Ap2_Brito.DAL
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data source = DATA\Parcial.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
